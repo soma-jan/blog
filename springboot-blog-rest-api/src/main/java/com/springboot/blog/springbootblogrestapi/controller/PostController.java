9ac40e42e8eb7275e39b3bd62fdac1f6bbbff7e0
soma <soma.j18@gmail.com>
2022-07-05 13:14:49 -0400

    post crud completed

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
@@ -24,4 +24,18 @@ public class PostController {
     public List<PostDto> getPost(){
         return postservice.getPost();
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto>getPostById(@PathVariable(name = "id") Long id){
+        return  ResponseEntity.ok(postservice.getPostById(id));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto>updatePostById(@RequestBody PostDto postdto ,@PathVariable(name = "id") Long id){
+    return new ResponseEntity<>(postservice.updatePost(postdto,id),HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String>deletePostById(@PathVariable(name = "id") Long id){
+            postservice.deletePostById(id);
+            return new ResponseEntity<>("Post entity of id:%s is deleted successfully",HttpStatus.OK);
+    }
+
 }
